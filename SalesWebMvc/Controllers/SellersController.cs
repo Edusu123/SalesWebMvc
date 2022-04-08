a7e9d26d2ecc7acf5a6d50b483cd9a4e5767f78c
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-04-07 21:25:14 -0300

    Simple register screen

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -13,12 +14,25 @@ namespace SalesWebMvc.Controllers
             _sellerService = sellerService;
         }
 
-
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
